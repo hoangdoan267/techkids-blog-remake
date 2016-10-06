ca165ccf476c27200e7d4aad5d9a497f0c3148ad
Hoang Doan <hoang.doan@outlook.com>
2016-10-07 00:31:58 +0700

    Sidebar

--- wp-content/themes/html5blank-stable/sidebar.php
+++ wp-content/themes/html5blank-stable/sidebar.php
@@ -1,15 +1,37 @@
 <!-- sidebar -->
-<aside class="sidebar" role="complementary">
-
-	<?php get_template_part('searchform'); ?>
-
-	<div class="sidebar-widget">
-		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
+<aside class="sidebar col-sm-4" role="complementary">
+	<div class="sidebar-title-group">
+		<div class="sidebar-title">
+			<h4>CHUYÊN MỤC</h4>
+		</div>		
 	</div>
-
-	<div class="sidebar-widget">
-		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
+	<div class="sidebar-category-list">
+		<ul>
+		<?php
+			$categories = array(
+				'orderby' => 'id',
+				'exclude' => '1',
+      			'hide_empty'=> 0
+			); 
+			$list = get_categories($categories);
+			foreach ($list as $cat):
+				$link = get_category_link($cat->term_id);
+		?>
+			<li><h5><a href="<?php echo esc_url($link);?>"><?php echo $cat->name?></a><h5></li>
+		<?php
+			endforeach;
+		?>
+		</ul>
 	</div>
-
+	<div class="sidebar-widget-form">
+		<div class="sidebar-widget-title">
+			<h4>Muốn đọc nhiều hơn nhưng bài viết hay từ Techkids?</h4>
+			<h3>Đăng ký theo dõi nhé!</h3>
+		</div>
+		<form class="sidebar-form">
+				<input type="text" name="email" placeholder="Email">
+				<button type="submit" class="btn btn-default">Đăng kí</button>
+		</form>
+	</div>
 </aside>
 <!-- /sidebar -->
--- wp-content/themes/html5blank-stable/single.php
+++ wp-content/themes/html5blank-stable/single.php
@@ -1,16 +1,14 @@
 <?php get_header(); ?>
 
 	<div class="container">
-		<div class="row">
+		<div class="row single-page-wrapper">
 			<div class="col-sm-8 col-xs-12">
 				<article>
 					<!-- TODO Single Content -->
 				</article>
 				
 			</div>
-			<div class="col-sm-4">
-				<?php echo "Hello World"; ?>
-			</div>
+				<?php get_sidebar(); ?>
 		</div>
 	</div>
 
